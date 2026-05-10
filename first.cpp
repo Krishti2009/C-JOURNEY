c4efbff05411ce28df4a6ba99f9cf67c5ae0289f
Krishti Lamichhane <kristhilamichhane@gmail.com>
2026-05-10 21:43:15 +0545

    C++ journey begins here

---
+++ first.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// #include <string>
+// int main()
+// {
+//     std::string name = "Krishti Lamichhane";
+//     int age = 19;
+//     double gpa = 3.98;
+//     char grade = 'A';
+//     bool isstd = true;
+//     std::cout << "Hello" << name << std::endl;
+//     std::cout << "Age=" << age << std::endl;
+//     std::cout << "gpa=" << gpa << std::endl;
+//     std::cout << "grade=" << grade << std::endl;
+//     std::cout << "student=" << isstd << std::endl;
+//     return 0;
+// }
+#include <iostream>
+#include <string>
+using str_t = std::string;
+int main()
+{
+    str_t name = "Kanchu";
+    std::cout << name;
+    return 0;
+}
